95655db33b19ce110b14d517b7c5fab2709f1f52
Shixiong.Liu <635672377@qq.com>
2022-08-02 19:20:14 +0800

    optimized: cleanup code.

--- WinAssistant/WinAssistant.cpp
+++ WinAssistant/WinAssistant.cpp
@@ -11,28 +11,38 @@
 using std::string;
 using std::wstring;
 
+#define HWND_TO_WSTR(wnd) (std::to_wstring((int)wnd))
+#define HWND_TO_PWCHAR(wnd) (std::to_wstring((int)wnd).c_str())
+
+#define PROCESS_ID_TO_WSTR(id) (std::to_wstring((unsigned long)id))
+#define PROCESS_ID_TO_PWCHAR(id) (std::to_wstring((unsigned long)id).c_str())
+
 HHOOK g_hHook = NULL;
 extern HINSTANCE g_hInstDll;
 wstring title_cache;
 
 void Test(PBYTE pPayload, UINT64 size) {}
 
-void Send(const wchar_t *title) {
-  size_t size = wcslen(title) * sizeof(wchar_t);
+void Send(const wchar_t *title, const wchar_t *hwnd) {
+  wchar_t buff[MAX_PATH] = {0};
+  _snwprintf_s(buff, MAX_PATH, L"{\"title\":\"%s\", \"HWND\":\"%s\"}", title, hwnd);
+  size_t size = wcslen(buff) * sizeof(wchar_t);
   wstring temp_str = title;
   if (title_cache != temp_str) {
     title_cache = temp_str;
     Client client;
     client.init(dll_shm_name, MAX_SHM_SIZE, dll_evt_name);
-    client.send(function_name, (PVOID)title, size, Test);
+    client.send(function_name, buff, size, Test);
   }
 }
 
-void Quit(const wchar_t *process_id) {
-  size_t size = wcslen(process_id) * sizeof(wchar_t);
+void Quit(const wchar_t *process_id, const wchar_t *hwnd) {
+  wchar_t buff[MAX_PATH] = {0};
+  _snwprintf_s(buff, MAX_PATH, L"{\"process_id\":\"%s\", \"HWND\":\"%s\"}", process_id, hwnd);
+  size_t size = wcslen(buff) * sizeof(wchar_t);
   Client client;
   client.init(dll_shm_name, MAX_SHM_SIZE, dll_evt_name);
-  client.send(wnd_exit_name, (PVOID)process_id, size, Test);
+  client.send(wnd_exit_name, (PVOID)buff, size, Test);
 }
 
 LRESULT WINAPI CallWndProc(int nCode, WPARAM wParam, LPARAM lParam) {
@@ -41,9 +51,9 @@ LRESULT WINAPI CallWndProc(int nCode, WPARAM wParam, LPARAM lParam) {
   static long bFirst = 1;
   if (bFirst) {
     InterlockedExchange(&bFirst, 0);
-    wchar_t title[255] = {0};
-    GetWindowTextW(msg->hwnd, title, 255);
-    Send(title);
+    wchar_t title[MAX_PATH] = {0};
+    GetWindowTextW(msg->hwnd, title, MAX_PATH);
+    Send(title, HWND_TO_PWCHAR(msg->hwnd));
   }
 
   if (WM_SETTEXT == msg->message) {
@@ -51,14 +61,14 @@ LRESULT WINAPI CallWndProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (nullptr != title) {
       if (0 == wcslen(title)) {
         OutputDebugStringA("empty title");
-        Quit(std::to_wstring(GetCurrentProcessId()).c_str());
+        Quit(PROCESS_ID_TO_PWCHAR(GetCurrentProcessId()), HWND_TO_PWCHAR(msg->hwnd));
       } else {
-        Send(title);
+        Send(title, HWND_TO_PWCHAR(msg->hwnd));
       }
     }
   } else if (WM_CLOSE == msg->message || WM_QUIT == msg->message) {
     OutputDebugStringA("quit wnd");
-    Quit(std::to_wstring(GetCurrentProcessId()).c_str());
+    Quit(PROCESS_ID_TO_PWCHAR(GetCurrentProcessId()), HWND_TO_PWCHAR(msg->hwnd));
   }
   return (CallNextHookEx(g_hHook, nCode, wParam, lParam));
 }
