4f21f514c8cb7ff9e3b6fff25096d56894dfb030
shixiong.Liu <635672377@qq.com>
2023-04-24 20:06:23 +0800

    WIP(dolphin): add about demo code

--- dolphin/dolphin.cpp
+++ dolphin/dolphin.cpp
@@ -17,16 +17,25 @@ using std::string;
 // }
 // 2、默认读取当前目录下的config.json
 int main(int argc, char *argv[]) { 
-    string path = "./config.json";
-    if (argc > 1)
-        path = argv[1];
+    //string path = "./config.json";
+    //if (argc > 1)
+    //    path = argv[1];
 
-    std::ifstream ifs;
-    ifs.open(path, std::ios::in);
-    if (!ifs.is_open()) {
-        std::cout << "Open file failed." << std::endl;
-        return -1;
-    }
+    //std::ifstream ifs;
+    //ifs.open(path, std::ios::in);
+    //if (!ifs.is_open()) {
+    //    std::cout << "Open file failed." << std::endl;
+    //    return -1;
+    //}
     // ifs.rad();
+
+    PEVersionInfo info;
+    info.loadFromPEFile(R"(E:\my_document\platypus\bin_win32\Release\platypus.exe)");
+    std::wstring version = info.getFileVersion();
+
+    info.setFileVersion(L"1.0.0.300");
+    info.setFileDescription(L"我是测试行文字01");
+
+    info.saveToPEFile(R"(E:\my_document\platypus\bin_win32\Release\platypus.exe)");
     return 0; 
 }
