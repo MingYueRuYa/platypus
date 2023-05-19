65cf8f8f1e68d955bedff44f974e396f6573c165
shixiong.Liu <635672377@qq.com>
2023-05-19 21:56:02 +0800

    chore(package): add about package code

--- dolphin/dolphin.cpp
+++ dolphin/dolphin.cpp
@@ -1,41 +1,48 @@
 ﻿#include <iostream>
 #include <string>
 #include <fstream>
+#include <json.hpp>
 
 #include "PEVersionInfo.h"
+#include "string_utils.hpp"
 
+using json = nlohmann::json;
 using std::string;
 
 // https://github.com/blackknifes/PEVersionInfo
 
-//TODO：1、读取配置文件，批量修改exe信息
-//  {
-//     "exe_name": {
-//         "FileVersion":1.0.0.200,
-//         "CopyRight":"我是测试性文字",
-//     }
-// }
-// 2、默认读取当前目录下的config.json
-int main(int argc, char *argv[]) { 
-    //string path = "./config.json";
-    //if (argc > 1)
-    //    path = argv[1];
+// TODO：1、读取配置文件，批量修改exe信息
+//   {
+//          "FileVersion":1.0.0.200,
+//          "CopyRight":"我是测试性文字",
+//          "ProductVersion":1.0.0.200,
+//  }
+//  2、默认读取当前目录下的config.json
+int main(int argc, char *argv[]) {
+  if (argc < 3) {
+    std::cout << "usage: app config_path exe_path" << std::endl;
+    return 0;
+  }
+  std::ifstream f("example.json");
+  json data = json::parse(f);
+  if (data.is_null()) {
+    std::cout << "parse json data error" << std::endl;
+    return -1;
+  }
 
-    //std::ifstream ifs;
-    //ifs.open(path, std::ios::in);
-    //if (!ifs.is_open()) {
-    //    std::cout << "Open file failed." << std::endl;
-    //    return -1;
-    //}
-    // ifs.rad();
+  PEVersionInfo info;
+  info.loadFromPEFile(argv[2]);
 
-    PEVersionInfo info;
-    info.loadFromPEFile(R"(E:\my_document\platypus\bin_win32\Release\platypus.exe)");
-    std::wstring version = info.getFileVersion();
+  string value = data.value("FileVersion", "");
+  info.setFileVersion(to_wide_string(value));
+  value = data.value("Copyright", "");
+  info.setFileDescription(to_wide_string(value));
 
-    info.setFileVersion(L"1.0.0.300");
-    info.setFileDescription(L"我是测试行文字01");
-
-    info.saveToPEFile(R"(E:\my_document\platypus\bin_win32\Release\platypus.exe)");
-    return 0; 
+  info.saveToPEFile(argv[2]);
+  std::cout << "update exe info successful" << std::endl;
+  std::wstring version = info.getFileVersion();
+  std::wstring desc = info.getFileDescription();
+  std::wcout << L"file version:" << info.getFileVersion()
+             << " file desc:" << info.getFileDescription() << std::endl;
+  return 0;
 }
