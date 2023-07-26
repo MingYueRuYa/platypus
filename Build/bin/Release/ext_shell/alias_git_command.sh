609e55f8dcbc682eff1d21e39b634c956d26d5e3
shixiong.Liu <635672377@qq.com>
2023-07-26 19:39:26 +0800

    refactor(script): add clean alias

--- Build/bin/Release/ext_shell/alias_git_command.sh
+++ Build/bin/Release/ext_shell/alias_git_command.sh
@@ -5,7 +5,8 @@ command_arr=("alias count_id='git rev-list --count '" "alias git_all_log='git lo
 		 	 "alias fetch='git fetch'" "alias tortise_log='tortoisegitproc.exe /command:log'"
 		 	 "alias tortise_commit='tortoisegitproc.exe /command:commit'"
 		 	 "alias git_st='source git_st'" "alias commit='git commit -m '"
-		 	 "alias add='git add'" "alias checkout='git checkout'")
+		 	 "alias add='git add'" "alias checkout='git checkout'"
+		 	 "alias clean='git clean -x -f -q'")
 
 source /usr/bin/echo_color_func.sh
 
