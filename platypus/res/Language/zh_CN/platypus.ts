a2f127250c25023c6e4630809c65a87bb580807f
shixiong.Liu <635672377@qq.com>
2023-08-02 20:24:41 +0800

    translator(add): add about lanuage files

---
+++ platypus/res/Language/zh_CN/platypus.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>ButtonWidget</name>
+    <message>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>HelpDialog</name>
+    <message>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Author</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>email: studycpp1024@gmail.com 635672377@qq.com</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>wechat public account：cpp手艺人</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Shortcut</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>create new tab：CTRL+SHIFT+A</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>delete tab: CTRL+SHIFT+W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>switch forward Tab: CTRL+SHIFT+TAB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>switch back Tab: CTRL+TAB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>maximum/minimize window: ALT+F11</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Platypus</name>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not find git install path</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>title</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>already start same process</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>platypusClass</name>
+    <message>
+        <source>Platypus</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
